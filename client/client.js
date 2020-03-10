221267102e18093003c2ce54596e3f2d1fb4eedb
Conner McCabe <conner.p.mccabe@gmail.com>
2020-03-10 15:57:55 -0500

    displaying messages properly

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const messageElement = document.querySelector('.messages');
 const API_URL = 'http://localhost:3000/messages';
 
 loadingElement.style.display = ""; //none
@@ -16,8 +17,8 @@ form.addEventListener('submit', (event) =>{
         name,
         content
     };
-    form.style.display = 'none';
-    loadingElement.style.display = '';
+    form.style.display = '';
+    loadingElement.style.display = 'none';
 
     fetch(API_URL, {
         method: 'POST',
@@ -30,16 +31,37 @@ form.addEventListener('submit', (event) =>{
         console.log(createdMessage)
         form.reset();
         form.style.display = '';
+        listAllMessages();
         loadingElement.style.display = 'none';
     })
     
 })
-
+// list all messages, creates div element below the loading bar
+// with a name header and a content p tag
 function listAllMessages() {
+    messageElement.innerHTML = '';
     fetch(API_URL)
         .then(response => response.json())
         .then(messages => {
             console.log(messages)
-            messages.forEach(message)
+            messages.reverse();
+            messages.forEach(message => {
+                const div = document.createElement('div')
+
+                const header = document.createElement('h3');
+                header.textContent = message.name;
+
+                const content = document.createElement('p');
+                content.textContent = message.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(message.created);
+
+                div.appendChild(header);
+                div.appendChild(content);
+                div.appendChild(date);
+                messageElement.appendChild(div);
+            });
+            loadingElement.style.display = 'none';
         });
 }
